1554890c9e954a783b5c10c5587ad07f96583f14
Zantos321 <eljoe1985@gmail.com>
2020-05-07 16:20:23 -0700

    button fixes succes and error displays

---
+++ script.js
@@ -0,0 +1,7 @@
+$("#save-imagery").click(function () {
+   $("#overlay-success").toggleClass("d-flex d-none");
+});
+
+$("#save-error").click(function () {
+   $("#overlay-error").toggleClass("d-flex d-none");
+});
