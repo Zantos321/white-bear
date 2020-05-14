354a37dfd0baafab31ed4b2106007a9264b37791
Zantos321 <eljoe1985@gmail.com>
2020-05-13 17:20:52 -0700

    made login errors and started on text counter pain

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+// SIGN UP VALIDATION
+
+$("#signupVerify").click(function () {
+   var emailInput = $("#signupEmail").val().length;
+   if (emailInput == 0) {
+      $("#signupEmailAlert").removeClass("d-none");
+      return false;
+   } else {
+      $("#signupEmailAlert").addClass("d-none");
+      return true;
+   }
+});
+
+$("#signupVerify").click(function () {
+   var passwordInput = $("#signupPassword").val().length;
+   if (passwordInput < 9) {
+      $("#signupPasswordAlert").removeClass("d-none");
+      return false;
+   } else {
+      $("#signupPasswordAlert").addClass("d-none");
+      return true;
+   }
+});
+
+// LOGIN VALIDATION
+
+$("#loginVerify").click(function () {
+   var emailInput = $("#loginEmail").val().length;
+   if (emailInput == 0) {
+      $("#emailAlert").removeClass("d-none");
+      return false;
+   } else {
+      $("#emailAlert").addClass("d-none");
+      return true;
+   }
+});
+
+$("#loginVerify").click(function () {
+   var passwordInput = $("#loginPassword").val().length;
+   if (passwordInput < 9) {
+      $("#passwordAlert").removeClass("d-none");
+      return false;
+   } else {
+      $("#passwordAlert").addClass("d-none");
+      return true;
+   }
+});
+
 // SAVE SUCCESS AND SAVE ERROR NOTIFICATIONS
 
 $("#save-imagery").click(function () {
@@ -12,4 +60,13 @@ $("#save-error").click(function () {
 
 $("#delete-check").click(function () {
    $("#card-delete").toggleClass("invisible");
+});
+
+// TEXTAREA COUNTER
+
+$("#cardText").keyup(function () {
+   var textCount = $(this).val().length;
+   var current = $("#wordCount");
+
+   current.text(textCount);
 });
