088f86ddd7d64eb0cffdbbc5e8f235cbefbce7ab
Zantos321 <eljoe1985@gmail.com>
2020-05-18 09:35:49 -0700

    made date function and fixed formatting

--- script.js
+++ script.js
@@ -46,31 +46,12 @@ $("#signupVerify").click(function () {
 
 $("#signupVerify").click(function () {
    if ((emailVerify = true)) {
-      var now = new Date();
-      var randomNumber = Math.floor(Math.random() * 100);
-      var outputRandom = randomNumber.padStart(3, "0");
-
-      var idNum =
-         ("" + now.getMilliseconds()).padStart(3, "0") + "" + outputRandom;
       var emailLog = $("#signupEmail").val();
       var passwordLog = $("#signupPassword").val();
-      var createdOn =
-         "" +
-         now.getYear() +
-         "" +
-         (now.getMonth() + 1) +
-         "" +
-         now.getDay() +
-         "" +
-         now.getHours() +
-         "" +
-         now.getMinutes() +
-         "" +
-         now.getSeconds();
-      console.log("_id: " + idNum);
+      console.log("_id: " + IDnumber());
       console.log("email: " + emailLog);
       console.log("password: " + passwordLog);
-      console.log("createdOn: " + createdOn);
+      console.log("createdOn: " + createdDate());
    }
 });
 
@@ -145,3 +126,40 @@ $("#cardTextsm").keyup(function () {
       $("#wordMaximum").removeClass("text-danger");
    }
 });
+
+// ID AND DATE AND TIME FUNCTIONS
+
+function IDnumber() {
+   // Get current date
+   var now = new Date();
+   // Generate first half of ID
+   var getMillisecondsID = ("" + now.getMilliseconds()).padStart(3, "0");
+   // Random 3 digit number for second half of ID
+   var randomNumber = Math.floor(Math.random() * 1000);
+   // Turn random number into a string
+   var randomNumberString = String(randomNumber);
+   // Pad string to have at least 3 digits
+   var outputRandom = randomNumberString.padStart(3, "0");
+   return getMillisecondsID + outputRandom;
+}
+
+function createdDate() {
+   var now = new Date();
+   var yearFull = String(now.getFullYear());
+   var year = yearFull.substring(2);
+   // Unpadded dates
+   var monthNoPad = String(now.getMonth() + 1);
+   var dayNoPad = String(now.getDay());
+   var hourNoPad = String(now.getHours());
+   var minutesNoPad = String(now.getMinutes());
+   var secondsNoPad = String(now.getSeconds());
+   // Padded dates
+   var month = monthNoPad.padStart(2, "0");
+   var day = dayNoPad.padStart(2, "0");
+   var hour = hourNoPad.padStart(2, "0");
+   var minutes = minutesNoPad.padStart(2, "0");
+   var seconds = secondsNoPad.padStart(2, "0");
+   // Everything concatenated together to make a date code
+   var createdOn = year + month + day + hour + minutes + seconds;
+   return createdOn;
+}
