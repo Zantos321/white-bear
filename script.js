b66fbbe38e75e2d759d6c1078dd430f38776d48d
Zantos321 <eljoe1985@gmail.com>
2020-05-18 08:46:06 -0700

    added email password verification

--- script.js
+++ script.js
@@ -1,24 +1,76 @@
 // SIGN UP VALIDATION
 
+var emailVerify = false;
 $("#signupVerify").click(function () {
+   // Check length of email
    var emailInput = $("#signupEmail").val().length;
+   // Run a check to either allow email or to show a warning under email
    if (emailInput == 0) {
       $("#signupEmailAlert").removeClass("d-none");
       return false;
    } else {
       $("#signupEmailAlert").addClass("d-none");
-      return true;
+      return (emailVerify = true);
    }
 });
 
+var passwordVerify = false;
 $("#signupVerify").click(function () {
+   // Check length of password
    var passwordInput = $("#signupPassword").val().length;
+   // Check password to see if met requirement and eityher allow password by length or show a warning under password
    if (passwordInput < 9) {
       $("#signupPasswordAlert").removeClass("d-none");
       return false;
    } else {
       $("#signupPasswordAlert").addClass("d-none");
-      return true;
+      return (passwordVerify = true);
+   }
+});
+
+var emailPasswordVerify = false;
+$("#signupVerify").click(function () {
+   // Get email local part
+   var emailLocal = $("#signupEmail").val().split("@")[0];
+   // Check to see if password and local email match
+   if ($("#signupPassword").val() === emailLocal) {
+      $("#passwordMatchesEmailAlert").removeClass("d-none");
+      $("#signupEmail").addClass("is-invalid");
+      return false;
+   } else {
+      $("#passwordMatchesEmailAlert").addClass("d-none");
+      $("#signupEmail").removeClass("is-invalid");
+      return (emailPasswordVerify = true);
+   }
+});
+
+$("#signupVerify").click(function () {
+   if ((emailVerify = true)) {
+      var now = new Date();
+      var randomNumber = Math.floor(Math.random() * 100);
+      var outputRandom = randomNumber.padStart(3, "0");
+
+      var idNum =
+         ("" + now.getMilliseconds()).padStart(3, "0") + "" + outputRandom;
+      var emailLog = $("#signupEmail").val();
+      var passwordLog = $("#signupPassword").val();
+      var createdOn =
+         "" +
+         now.getYear() +
+         "" +
+         (now.getMonth() + 1) +
+         "" +
+         now.getDay() +
+         "" +
+         now.getHours() +
+         "" +
+         now.getMinutes() +
+         "" +
+         now.getSeconds();
+      console.log("_id: " + idNum);
+      console.log("email: " + emailLog);
+      console.log("password: " + passwordLog);
+      console.log("createdOn: " + createdOn);
    }
 });
 
